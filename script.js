d878427353e0c1f2540146e58a955d0bcd7ce082
janiduDhakshitha <143499103+janiduDhakshitha@users.noreply.github.com>
2024-09-11 23:44:51 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const textBox = document.getElementById("textBox");
+const tofahrenheit = document.getElementById("tofahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+
+
+function convert(){
+
+if(tofahrenheit.checked){
+    temp = Number(textBox.value);
+    temp = temp * 9/5 +32;
+    result.textContent = `${temp.toFixed(1)} ℉`;
+    
+}else if(toCelsius.checked){
+    temp = Number(textBox.value);
+    temp = (temp-32) * 5/9;
+    result.textContent = `${temp.toFixed(1)} ℃`;
+}else{
+   result.textContent = "pick one unit to convert";
+}
+
+}
